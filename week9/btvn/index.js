33e7ad7c65cf8a86688a1b6e35d594d9a1e8cf70
Gina126 <thgng12@gmail.com>
2026-01-04 23:30:20 +0700

    btvn

---
+++ week9/btvn/index.js
@@ -0,0 +1,34 @@
+const nameInput = document.getElementById("nameInput");
+const scoreInput = document.getElementById("scoreInput");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("studentList");
+
+addBtn.addEventListener("click", () => {
+  const name = nameInput.value;
+  const score = parseFloat(scoreInput.value);
+
+  if (name === "" || isNaN(score) || score < 0 || score > 10) {
+    alert("Vui lòng nhập tên và điểm số hợp lệ (0-10)!");
+    return;
+  }
+
+  const li = document.createElement("li");
+
+  li.textContent = `${name} - ${score} điểm`;
+
+  if (score >= 5) {
+    li.style.color = "#4cd137";
+  } else {
+    li.style.color = "#e84118";
+  }
+
+  li.addEventListener("click", function () {
+    list.removeChild(this);
+  });
+
+  list.appendChild(li);
+
+  nameInput.value = "";
+  scoreInput.value = "";
+  nameInput.focus();
+});
---
+++ week9/trenlop/index.js
@@ -0,0 +1,39 @@
+// const allTitles = document.getElementsByTagName("h1");
+// /* Biến allTitles này là một HTMLCollections*/
+
+// console.log(allTitles); //HTMLCollections
+// console.log(allTitles.length); // Độ dài của HTMLCollections là 4
+
+// for (let i = 0; i < allTitles.length; i++) {
+//   console.log(allTitles[i]);
+// }
+// /* Sử dụng vòng lặp để duyệt qua các thành phần trong HTMLCollections*/
+// console.log(document);
+
+// let firstTitle = document.querySelector("h1"); // select the first available h1 element
+// // let firstTitle = document.querySelectorAll("h1"); // select all available h1 element
+// console.log(firstTitle);
+
+// const titles = document.querySelectorAll("h1");
+// titles[3].setAttribute("class", "title"); //class: "title"
+// titles[3].setAttribute("id", "fourth-title");
+
+// console.log(titles);
+// // //or
+// // const titles = document.querySelectorAll("h1");
+// // titles[3].className = "title";
+// // titles[3].id = "fourth-title";
+// // console.log(titles);
+
+// //another way to setting an attribute: append the class, doesn't over ride
+// const titles = document.querySelectorAll("h1");
+// titles[3].classList.add("title", "header-title");
+// console.log(titles);
+
+// const titles = document.querySelectorAll("h1");
+// titles[3].textContent = "Fourth Title";
+// // titles[3].innerText = "Fourth Title";
+
+// console.log(titles);
+
+//phan 7
